ff0f124d91d3a4beaf5a1d2f359dbc592c58d308
zjyu <work@zjyu.de>
2023-05-03 14:19:10 +0800

    用户名密码登录

--- userlogin.py
+++ userlogin.py
@@ -4,6 +4,35 @@ from PyQt5.QtCore import QCoreApplication
 from PyQt5.QtWidgets import QFrame, QVBoxLayout, QLineEdit, QPushButton, \
     QDialog, QMessageBox, QApplication
 
+from userlogin_exam import Ui_UserNameLogin
 
-class UserLoginDialog(QDialog):
-    pass
+
+class UserNameLoginDialog(QDialog, Ui_UserNameLogin):
+    """
+    用户名密码登录对话框，
+    使用 Qt Designer 设计，
+    继承自 Ui_UserNameLogin，实现显示与逻辑分离
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setupUi(self)
+        self.LoginBotton.clicked.connect(self.login_event)
+        self.QuitBotton.clicked.connect(QCoreApplication.instance().quit)
+
+    def login_event(self):
+        username = self.UserNameInput.text()
+        password = self.PasswdInput.text()
+
+        if username == 'admin' and password == 'hello':
+            # 临时测试逻辑 后续需要适配 登录 API TODO
+            self.accept()
+            return True
+        else:
+            QMessageBox.warning(
+                self, '错误', '用户名或密码错误！',
+                QMessageBox.Yes, QMessageBox.Yes
+            )
+
+            self.UserNameInput.setFocus()  # 将光标移动到用户名输入框
+            self.PasswdInput.selectAll()  # 将用户名输入框中的内容全选
+            return False
