29e7fc112f9626baffa5daad1e8e144da785d788
zjyu <work@zjyu.de>
2023-05-03 14:20:11 +0800

    user_main 作为用户名-密码登录的入口

--- user_main.py
+++ user_main.py
@@ -8,28 +8,31 @@ code file: first_demo.py
 
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog
-from gui import Ui_mainWindow
-import login
+from new_gui import MainGui
+import userlogin as login
 
 if __name__ == "__main__":
     # 创建应用
     window_application = QApplication(sys.argv)
     # 设置登录窗口
-    login_ui = login.LoginDialog()
+    login_ui = login.UserNameLoginDialog()
 
 
     # 校验是否验证通过
     if login_ui.exec_() == QDialog.Accepted:
-        username = login_ui.login_id.text()
+        username = login_ui.UserNameInput.text()
         print('username', username)
         # 初始化主功能窗口
         app = QApplication(sys.argv)
-        mainWindow = QMainWindow()
-        ui = Ui_mainWindow()
+        mainWindow = MainGui()
+        mainWindow.setupUi(mainWindow)
         # 登陆成功，修改主窗口的“当前用户” 和 “当前店铺” 的值
-        ui.setupUi(mainWindow)
-        ui.CurrentUserLabel.setText("当前用户： " + username)
+        # TODO 当前店铺
+        mainWindow.CurrentUserLabel.setText("当前用户： " + username)
         mainWindow.show()
+        mainWindow.listen_clipboard()
+        mainWindow.set_button()
         sys.exit(app.exec_())
+
     else:
         sys.exit(window_application.exec_())
