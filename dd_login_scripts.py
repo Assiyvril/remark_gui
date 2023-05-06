fb1957c2bbfc54d9ec547da7375f293e7d637e69
zjyu <work@zjyu.de>
2023-05-06 11:32:31 +0800

    扫码登录

--- dd_login_scripts.py
+++ dd_login_scripts.py
@@ -23,15 +23,17 @@ from PyQt5.QtWidgets import QMainWindow, QApplication
 APP_KEY = 'dingqqjuy2zdbf7qd9v0'
 APP_SECRET = '2DeitGN2KKvEMixQL_CO4tc-t0VTJRrGsuwP9R5AdM0XSPpNUDqJ7g2NuIFxlEu5'
 AGENT_ID = '2568548056'
-TEM_ACCESS_TOKEN = 'e074889a23493072bc6bd768485d8c92'
+TEM_ACCESS_TOKEN = '6a94830698473ea7bc919ae825801c87'
 QR_URL = 'https://oapi.dingtalk.com/connect/qrconnect?appid=dingqqjuy2zdbf7qd9v0&response_type=code&scope=snsapi_login&state=123&redirect_uri=http://127.0.0.1:80'
 
 
 class DDLogin:
 
-    def __init__(self):
-        self.access_token = 'e074889a23493072bc6bd768485d8c92'
+    def __init__(self, sns_code):
+        self.access_token = TEM_ACCESS_TOKEN
         self.user_token_dict = None
+        self.sns_code = sns_code
+        # self.get_access_token()
 
     def get_access_token(self):
         """
@@ -91,15 +93,54 @@ class DDLogin:
         else:
             return None
 
-    def get_sns(self):
+    def get_user_info(self):
         """
-        获取用户授权的SNS_Code
+        根据 SNS code 获取用户信息
+        :param sns_code:
         :return:
         """
-        url = f'https://oapi.dingtalk.com/connect/qrconnect?appid={APP_KEY}&response_type=code&scope=snsapi_login&state=STATE&redirect_uri=http://ddt.zjyu.de'
-        response = requests.get(url=url)
-        print(response.text)
-        return None
+        time_stamp = int(time.time() * 1000)
+        # 签名算法为HmacSHA256，签名数据是当前时间戳timestamp，密钥是 APP_SECRET，使用密钥对 timestamp 计算签名值。
+
+        signature_bytes = hmac.new(APP_SECRET.encode('utf-8'),
+                                   str(time_stamp).encode('utf-8'),
+                                   sha256).digest()
+
+        signature = base64.b64encode(signature_bytes).decode('utf-8')
+        import urllib.parse
+        signature = urllib.parse.quote_plus(signature)
+        url = f'https://oapi.dingtalk.com/sns/getuserinfo_bycode?accessKey=dingqqjuy2zdbf7qd9v0&timestamp={time_stamp}&signature={signature}'
+        post_data = json.dumps(
+            {'tmp_auth_code': self.sns_code}
+        )
+        header = {
+            'Content-Type': 'application/json'
+        }
+        response = requests.post(
+            url=url,
+            data=post_data,
+            headers=header
+        )
+        response_data = response.json()
+        return response_data
+
+    def get_user_id(self, unionid):
+        """
+        通过 unionid 获取用户 userid
+        :return:
+        """
+        url = 'https://oapi.dingtalk.com/user/getUseridByUnionid'
+        query_params = {
+            'access_token': self.access_token,
+            'unionid': unionid
+        }
+        response = requests.get(
+            url=url,
+            params=query_params
+        )
+        response_data = response.json()
+        print('user_id_info \n', response_data)
+        return response_data
 
 
 if __name__ == '__main__':
@@ -111,12 +152,15 @@ if __name__ == '__main__':
         def interceptRequest(self, info):
             url = info.requestUrl().toString()
             print('url:', url)
-            auth_code = re.findall(r'code=(.*?)&', url)
-            print('auth_code:', auth_code)
-
-            if auth_code:
-                print('发现code:', auth_code)
-                dd_response = self.get_user_info(auth_code[0])
+            sns_code = re.findall(r'code=(.*?)&', url)
+            print('sns_code:', sns_code)
+            if sns_code:
+                print('发现code:', sns_code)
+                dd_login_obj = DDLogin(sns_code[0])
+                print('实例化dd_login_obj成功')
+                print('access_token:', dd_login_obj.access_token)
+                print('sns_code:', dd_login_obj.sns_code)
+                dd_response = dd_login_obj.get_user_info()
                 if dd_response.get('errcode') != 0:
                     return False
                 elif dd_response.get('errmsg') != 'ok':
@@ -125,53 +169,10 @@ if __name__ == '__main__':
                 if not user_info:
                     return False
                 union_id = user_info.get('unionid')
-                dd_id = user_info.get('dingId')
-                openid = user_info.get('openid')
-                print('union_id:', union_id)
-                print('dd_id:', dd_id)
-                print('openid:', openid)
+                dd_login_obj.get_user_id(union_id)
 
             else:
                 pass
-
-        def get_user_info(self, sns_code):
-            """
-            根据 SNS code 获取用户信息
-            :param sns_code:
-            :return:
-            """
-            time_stamp = int(time.time() * 1000)
-            # 签名算法为HmacSHA256，签名数据是当前时间戳timestamp，密钥是 APP_SECRET，使用密钥对 timestamp 计算签名值。
-
-            signature_bytes = hmac.new(APP_SECRET.encode('utf-8'),
-                                       str(time_stamp).encode('utf-8'),
-                                       sha256).digest()
-
-            signature = base64.b64encode(signature_bytes).decode('utf-8')
-            import urllib.parse
-            signature = urllib.parse.quote_plus(signature)
-            url = f'https://oapi.dingtalk.com/sns/getuserinfo_bycode?accessKey=dingqqjuy2zdbf7qd9v0&timestamp={time_stamp}&signature={signature}'
-            post_data = json.dumps(
-                {'tmp_auth_code': sns_code}
-            )
-            header = {
-                'Content-Type': 'application/json'
-            }
-            response = requests.post(
-                url=url,
-                data=post_data,
-                headers=header
-            )
-            response_data = response.json()
-            return response_data
-
-        # def get_user_id(self, union_id):
-        #     """
-        #     根据 union_id 获取用户 id
-        #     :param union_id:
-        #     :return:
-        #     """
-        #     url = 'https://oapi.dingtalk.com/user/getUseridByUnionid'
 
     app = QApplication(sys.argv)
     view = QWebEngineView()
